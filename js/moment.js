a87d6aa2be84037ceda5b401b3d64ee0e63e761b
Kushal Sarkar <connect.kushals@gmail.com>
2026-07-07 20:13:33 +0530

    Implemented new Moments Page

--- js/moment.js
+++ js/moment.js
@@ -6,6 +6,7 @@ const moments = {
         profile: "images/profiles/alex.webp",
         caption: "Finally the weekend 😌"
     },
+
     emma: {
         name: "Emma",
         time: "1 hour ago",
@@ -13,6 +14,7 @@ const moments = {
         profile: "images/profiles/emma.webp",
         caption: "Coffee before coding ☕"
     },
+
     john: {
         name: "John",
         time: "Yesterday",
@@ -21,31 +23,44 @@ const moments = {
         caption: "Gaming night with friends 🎮"
     }
 };
-const momentView = document.getElementById("momentView");
+const storyName = document.getElementById("storyName");
+const storyTime = document.getElementById("storyTime");
+const storyImage = document.getElementById("storyImage");
+const storyCaption = document.getElementById("storyCaption");
+const storyAvatar = document.querySelector(".story-avatar");
+
+const replyInput = document.querySelector(".story-reply input");
+
 document.querySelectorAll(".story-item").forEach(item => {
+
     item.addEventListener("click", () => {
+
         document
             .querySelectorAll(".story-item")
             .forEach(card => card.classList.remove("active"));
-            item.classList.add("active");
-            const user = item.dataset.user;
-            const moment = moments[user];
-            momentView.innerHTML = `
-            <div class="moment-display">
-                <div class="display-header">
-                    <img src="${moment.profile}" class="display-avatar">
-                    <div>
-                        <h2>${moment.name}</h2>
-                        <span>${moment.time}</span>
-                    </div>
-                </div>
-                <img src="${moment.image}" class="display-photo">
-                <p class="display-caption">
-                    ${moment.caption}</p>
-                <button class="reply-btn">
-                Reply
-                </button>
-            </div>
-        `;
+
+        item.classList.add("active");
+
+        const user = item.dataset.user;
+
+        const moment = moments[user];
+
+        storyName.textContent = moment.name;
+        storyTime.textContent = moment.time;
+        storyCaption.textContent = moment.caption;
+        storyImage.src = moment.image;
+        storyAvatar.src = moment.profile;
+
+        replyInput.placeholder = `Reply to ${moment.name}...`;
+
+        document
+        .querySelectorAll(".progress")
+        .forEach(bar => bar.classList.remove("active"));
+        
+        document
+        .querySelector(".progress")
+        .classList.add("active");
+
     });
+
 });
